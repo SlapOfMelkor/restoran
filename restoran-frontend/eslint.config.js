28a612b656d86205fadc9c3869f5c20b21ba7804
SlapOfMelkor <bnmirfan19@gmail.com>
2025-12-17 15:06:41 +0300

    ts config updated

--- restoran-frontend/eslint.config.js
+++ restoran-frontend/eslint.config.js
@@ -19,5 +19,16 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'no-unused-vars': 'off', // TypeScript versiyonunu kullan
+    },
   },
 ])
